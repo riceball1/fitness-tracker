7d51468a97c09ed59ca8f861d0cd761ffd509cd9
Dana Ng <ngdana1@gmail.com>
2021-03-31 21:14:31 -0700

    Update grid

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import "./App.css";
 
 function App() {
   const GRID_ROW_LENGTH = 7;
-  const GRID_COL_LENGTH = 10;
+  const GRID_COL_LENGTH = 11;
  
   const grid = [];
   for (let row = 0; row < GRID_ROW_LENGTH; row++) {
     const currentRow = [];
     for (let col = 0; col < GRID_COL_LENGTH; col++) {
       const id = row + (col * GRID_ROW_LENGTH) + 1; 
-      currentRow.push(<div className="box" id={id}>{id}</div>);
+      if(id <=75) {
+        currentRow.push(<div className="box" id={id}>{id}</div>);
+      } else {
+        currentRow.push(<div className="box" id={id}></div>)
+      }
     }
     grid.push(currentRow);
   }
