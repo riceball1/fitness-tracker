18bde39dbfc65cb3d6528a96e767a63716a0b34d
Dana Ng <ngdana1@gmail.com>
2021-03-31 21:09:02 -0700

    Setup grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const GRID_ROW_LENGTH = 7;
+  const GRID_COL_LENGTH = 10;
+ 
+  const grid = [];
+  for (let row = 0; row < GRID_ROW_LENGTH; row++) {
+    const currentRow = [];
+    for (let col = 0; col < GRID_COL_LENGTH; col++) {
+      const id = row + (col * GRID_ROW_LENGTH) + 1; 
+      currentRow.push(<div className="box" id={id}>{id}</div>);
+    }
+    grid.push(currentRow);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        75 Days Hard Challenge
+        <h1>75 Days Hard Challenge</h1>
       </header>
+      <main class="box-grid">
+        {grid.map((row, rowId) => {
+          return (
+            <div key={rowId}>
+              {row.map((node, nodeId) => {
+                return node;
+              })}
+            </div>
+          );
+        })}
+      </main>
     </div>
   );
 }
