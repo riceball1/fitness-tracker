08e3e366e638e824b6ce3f6a84ffa2749301a949
Dana Ng <ngdana1@gmail.com>
2021-04-14 16:00:07 -0700

    Removed unneeded code

--- src/App.js
+++ src/App.js
@@ -2,39 +2,13 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const GRID_ROW_LENGTH = 7;
-  const GRID_COL_LENGTH = 11;
- 
-  const grid = [];
-  for (let row = 0; row < GRID_ROW_LENGTH; row++) {
-    const currentRow = [];
-    for (let col = 0; col < GRID_COL_LENGTH; col++) {
-      const id = row + (col * GRID_ROW_LENGTH) + 1; 
-      if(id <=75) {
-        currentRow.push(<div className="box" id={id}>{id}</div>);
-      } else {
-        currentRow.push(<div className="box" id={id}></div>)
-      }
-    }
-    grid.push(currentRow);
-  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>75 Days Hard Challenge</h1>
+        <h1>Fitness Tracker</h1>
       </header>
-      <main class="box-grid">
-        {grid.map((row, rowId) => {
-          return (
-            <div key={rowId}>
-              {row.map((node, nodeId) => {
-                return node;
-              })}
-            </div>
-          );
-        })}
-      </main>
+
     </div>
   );
 }
